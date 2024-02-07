09c7019128d1a3872860976168aec7174faf7a3d
Felipe Moura <felipemimoura@gmail.com>
2024-02-07 17:41:37 -0300

    feat: add  routes to categories module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'categories',
+    loadChildren: () => import('./pages/categories/categories-routing.module').then((m) => m.CategoriesRoutingModule)
+  },
+];
