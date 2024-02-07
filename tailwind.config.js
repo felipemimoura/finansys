ab56ecce446225850e28c32fc351f5a66f75dfc1
Felipe Moura <felipemimoura@gmail.com>
2024-02-07 17:12:17 -0300

    chore: config basic to project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js", // add this lin
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin")({ charts: true }), // add
+  ],
+};
