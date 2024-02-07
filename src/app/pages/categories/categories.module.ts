b891361a5af0d00994a303340ea2a0b1cd56f44a
Felipe Moura <felipemimoura@gmail.com>
2024-02-07 17:41:12 -0300

    feat: create category module

---
+++ src/app/pages/categories/categories.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CategoriesRoutingModule } from './categories-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CategoriesRoutingModule],
+})
+export class CategoriesModule {}
